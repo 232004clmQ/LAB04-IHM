19331ccf190dcb103151a06a0de3b64492e16c6d
Camila Moreno <166523772+232004clmQ@users.noreply.github.com>
2024-04-23 16:19:02 +0000

    fin

--- src/header.js
+++ src/header.js
@@ -2,10 +2,14 @@ const Header = () => {
     return  <div className="bg-orange">
     <header className="d-flex justify-content-center py-3">
     <ul className="nav nav-pills">
-        <li className="nav-item"><a href="#" className="nav-link text-dark">Inicio</a></li>
-        <li className="nav-item"><a href="#" className="nav-link text-dark">Servicios</a></li>
-        <li className="nav-item"><a href="#" className="nav-link text-dark">Nosotros</a></li>
-        <li className="nav-item"><a href="#" className="nav-link text-dark">Centro de Ayuda</a></li>
+        <li className="nav-item"><a href="#" className="nav-link text-dark">
+        <i data-feather="home"></i> Inicio</a></li>
+        <li className="nav-item"><a href="#" className="nav-link text-dark">
+        <i data-feather="briefcase"></i> Servicios</a></li>
+        <li className="nav-item"><a href="#" className="nav-link text-dark">
+        <i data-feather="users"></i> Nosotros</a></li>
+        <li className="nav-item"><a href="#" className="nav-link text-dark">
+        <i data-feather="help-circle"></i> Centro de Ayuda</a></li>
     </ul>
 </header>
 </div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css'
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Header from "./header";
 import Main from "./main";
 import Aside from "./aside";
 import Footer from "./footer";
+
+
+
+const feather = require('feather-icons');
+
+setTimeout ( () => {
+    feather.replace();
+}, 1000);
 
 const root = createRoot(document.getElementById("root"));
 root.render(
